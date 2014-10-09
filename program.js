1783d4177eb6b00e517bee03a76a0987abb92d16
kevinparkerson <kparkerson@exacttarget.com>
2014-10-08 21:53:52 -0400

    learnyounode - lesson 3: passed

--- program.js
+++ program.js
@@ -1,5 +1,6 @@
-var n = 0;
-for(var i=2; i<process.argv.length; i++){
-	n += Number(process.argv[i]);
-}
-console.log(n);
+var fs = require('fs');
+
+var fileString = fs.readFileSync(process.argv[2]).toString();
+var newLines = fileString.split('\n');
+
+console.log(newLines.length-1);
